d456b1083534602cb2d7e39c8400b8c747a98440
jgalicia <jgalicia-2022203@kinal.edu.gt>
2024-08-27 08:42:01 -0600

    build: se añade el proyecto inicial

--- src/org/pablovillela/system/main.java
+++ src/org/pablovillela/system/main.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package org.pablovillela.system;
 
-/**
- *
- * @author informatica
- */
 public class main {
 
     /**

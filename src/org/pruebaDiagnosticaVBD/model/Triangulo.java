27c79b82b1ceed30f8f1e1a69356c5063af2d341
M0B7 <dgarcia-2023532@kinal.edu.gt>
2024-08-27 10:33:21 -0600

    feat(modelo):se termino la subclase de triangulo

--- src/org/pruebaDiagnosticaVBD/model/Triangulo.java
+++ src/org/pruebaDiagnosticaVBD/model/Triangulo.java
@@ -4,7 +4,26 @@ package org.pruebaDiagnosticaVBD.model;
 public abstract class Triangulo extends Forma {
     
     
-    private int base;
-    private int altura;
+    private double base;
+    private double altura;
     
+    
+    
+    public Triangulo(int base, int altura, String color){
+        super(color);
+        this.base = base;
+        this.altura = altura;
+        
+    }
+    
+    
+    @Override
+    public double calcularArea(){
+        return base*altura/2;
+    }
+    
+    @Override
+    public String toString(){
+        return "La base es de" + base + "la altura es de" + altura;
+    }
 }
